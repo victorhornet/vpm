274beac7531cd7d84c944ada67bfc3538390a9dd
Victor Hornet <victor@hrnt.co>
2023-12-04 11:45:36 +0200

    feat: improve tui list

--- src/tui.rs
+++ src/tui.rs
@@ -6,14 +6,14 @@ use std::{
 };
 
 use crossterm::{
-    event::{self, Event, KeyCode},
+    event::{self, Event, KeyCode, KeyEventKind},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
 use ratatui::{
     prelude::*,
-    widgets::{Block, Borders, List, ListItem},
+    widgets::{Block, Borders, List, ListItem, ListState},
 };
 
 use crate::Project;
@@ -36,24 +36,46 @@ fn run(
     terminal: &mut Terminal<CrosstermBackend<Stdout>>,
     projects: BTreeMap<usize, Project>,
 ) -> Result<(), Box<dyn Error>> {
+    let mut selected_project = 0usize;
     loop {
         terminal.draw(|frame| {
             // let greeting = Paragraph::new("Hello World!");
             let items = projects
                 .values()
-                .map(|p| ListItem::new(format!("{:02}: {}", p.id, p)))
+                .map(|p| {
+                    ListItem::new(format!(
+                        "{:02} | {} | {}",
+                        p.id,
+                        p.date,
+                        p.name.split('-').collect::<Vec<_>>().join(" ")
+                    ))
+                })
                 .collect::<Vec<_>>();
             let list = List::new(items)
                 .block(Block::default().title("Projects").borders(Borders::ALL))
                 .style(Style::default().fg(Color::White))
                 .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
                 .highlight_symbol(">>");
-            frame.render_widget(list, frame.size());
+            let mut list_state = ListState::default();
+            list_state.select(Some(selected_project));
+            frame.render_stateful_widget(list, frame.size(), &mut list_state);
         })?;
-        if event::poll(Duration::from_millis(250))? {
+        if event::poll(Duration::from_millis(1000))? {
             if let Event::Key(key) = event::read()? {
-                if KeyCode::Char('q') == key.code {
-                    break;
+                if key.kind == KeyEventKind::Press {
+                    match key.code {
+                        KeyCode::Char('q') => break,
+                        KeyCode::Up | KeyCode::Char('k') => {
+                            if selected_project == 0 {
+                                selected_project = projects.len();
+                            }
+                            selected_project -= 1;
+                        }
+                        KeyCode::Down | KeyCode::Char('j') => {
+                            selected_project = (selected_project + 1) % projects.len();
+                        }
+                        _ => {}
+                    }
                 }
             }
         }
